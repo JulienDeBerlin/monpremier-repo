4c77db1d63e4cf7a7b6f702a4a6f5385d060148d
JulienDeBerlin <julien_berthoud@yahoo.fr>
2018-04-15 11:18:39 +0200

    initial commit

---
+++ src/Hello.java
@@ -0,0 +1,15 @@
+public class Hello {
+    public static void main(String[] arg) {
+
+        System.out.println("Hello Julien!");
+        int myFirstNumber = (10+5)+(2*10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        int myDifference = 1000 - myTotal;
+        System.out.println(myFirstNumber);
+        System.out.println(myTotal);
+        System.out.println(myDifference);
+    }
+}
